05ff456f0bd83da821b7dbbbbf56c5f2bd58be16
timur <ololokzn@gmail.com>
2025-07-10 16:20:21 +0000

    add complite comparison workflow

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,6 +3,7 @@ use std::{cmp::Ordering, io};
 
 fn main() {
     let _secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("Computers guess:{_secret_number}");
     println!("Enter your guess:");
     let mut guess = String::new();
 
@@ -10,9 +11,16 @@ fn main() {
         .read_line(&mut guess)
         .expect("need to input valid value!");
 
-    print!("Your guess: {guess}");
+    let guess: i32 = guess
+        .trim()
+        .parse()
+        .expect("Please enter a valid number!!!");
 
-    match _secret_number.cmp(guess) {
+    println!("Your guess: {guess}");
+
+    println!();
+
+    match &guess.cmp(&_secret_number) {
         Ordering::Equal => println!("Equal"),
         Ordering::Greater => println!("Greater"),
         Ordering::Less => println!("Less"),
