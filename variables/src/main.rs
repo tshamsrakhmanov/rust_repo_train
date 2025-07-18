6d91637b3b211da98d9655b31f410d271e75c1a3
timur <ololokzn@gmail.com>
2025-07-18 16:53:57 +0000

    add usage of constants

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,9 @@
+const MY_SIMPLE_VARIABLE_CONSTANT: i32 = 60 * 60 * 4;
+
 fn main() {
-    let mut x = 5;
-    println!("Here is value of: {x}");
-    x = 6;
-    println!("Here is mutated value of : {x}");
+    let mut q1 = 5;
+    println!("Here is value of: {q1}");
+    q1 = 6;
+    println!("Here is mutated value of : {q1}");
+    println!("Here is my constant: {MY_SIMPLE_VARIABLE_CONSTANT}");
 }
