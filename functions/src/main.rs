cf030d71094b5358cf92a293e54405cd88b28719
timur <ololokzn@gmail.com>
2025-07-20 10:37:56 +0000

    functions: add callling of several inputs

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    new_function();
+    new_function(5, 'a');
 }
 
-fn new_function() {
-    println!("Here is hellow from another fucntion!");
+fn new_function(integer_value: i32, char_value: char) {
+    println!("Incoming integer: {integer_value}, incoming char value: {char_value}");
 }
