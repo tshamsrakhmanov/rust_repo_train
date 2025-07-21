adae946ab121611c5498bbd7eb35100de5e10959
timur <ololokzn@gmail.com>
2025-07-21 11:10:17 +0000

    refactoring

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,14 +1,14 @@
-fn main() {
-    new_function(5, 'a');
-
-    let value = new_function_with_return(14);
-    println!("{value}");
-}
-
 fn new_function(integer_value: i32, char_value: char) {
     println!("Incoming integer: {integer_value}, incoming char value: {char_value}");
 }
 
 fn new_function_with_return(input: i32) -> i32 {
     input + 5
+}
+
+fn main() {
+    new_function(5, 'a');
+
+    let value = new_function_with_return(14);
+    println!("{value}");
 }
