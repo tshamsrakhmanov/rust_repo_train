a1587e4cacd25b28d7aa2d14001444466732c3cd
timur <ololokzn@gmail.com>
2025-07-21 14:05:35 +0000

    add simple conditioning

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 15;
+
+    if x < 10 {
+        println!("X is lower than 10");
+    } else {
+        println!("X is higher than 10");
+    }
 }
