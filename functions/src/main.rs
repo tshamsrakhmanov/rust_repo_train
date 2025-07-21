ebab550064359c3784cb46979470a8dcbb6d2afa
timur <ololokzn@gmail.com>
2025-07-21 11:33:58 +0000

    new training - branches

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,14 +1,14 @@
-fn new_function(integer_value: i32, char_value: char) {
+fn func_no_return(integer_value: i32, char_value: char) {
     println!("Incoming integer: {integer_value}, incoming char value: {char_value}");
 }
 
-fn new_function_with_return(input: i32) -> i32 {
+fn func_with_return(input: i32) -> i32 {
     input + 5
 }
 
 fn main() {
-    new_function(5, 'a');
+    func_no_return(5, 'a');
 
-    let value = new_function_with_return(14);
+    let value = func_with_return(14);
     println!("{value}");
 }
