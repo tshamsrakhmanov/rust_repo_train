dff8d262f9a49d606464d46c3e1ff2cef6bb6fcc
timur <ololokzn@gmail.com>
2025-07-23 17:19:32 +0000

    add commmenting of burrowing process

--- burrowing/src/main.rs
+++ burrowing/src/main.rs
@@ -1,23 +1,30 @@
 fn main() {
+    // функция просто возвращает стринг
     let str1 = return_string();
     println!("{str1}");
 
+    // функция принтует то, что ей передали. передаём только в виде ссылки
     let str2 = String::from("String for printing inside function");
     print_str_by_referal(&str2);
 
+    // функиця добавляет суффикс и принтует
     let str3: String = String::from("String to modify and print");
     append_no_modify_print(&str3);
 
+    // фунция берет стрингу которую передали в виде ссылки, добавляет суффикс и префекс и
+    // возвразает НОВУЮ строку
     let str4: String = String::from("WORD");
     let str5 = add_prefix_suffix(&str4);
     println!("{str5}");
 
+    // функция МОДИФИЦИРУЕТ переданную переменуную - добавляет к ней суффикс
     let mut str5: String = String::new();
     str5.push_str("[Base value of variable]");
     modify_str_by_add_prefif(&mut str5);
     modify_str_by_add_prefif(&mut str5);
     println!("{str5}");
 
+    // функция МОДИФИЦИРУЕТ изначальную перемнную добавляя к ней вторую
     let mut str6: String = String::from("[base string]");
     let str7: String = String::from("[add by function]");
     append_str_to_str(&mut str6, &str7);
