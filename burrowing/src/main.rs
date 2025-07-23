bc3b5ef1e2b6bae148337b6c2d3a35e6654c3960
timur <ololokzn@gmail.com>
2025-07-23 12:08:56 +0000

    add printing from funciton with append and no modification

--- burrowing/src/main.rs
+++ burrowing/src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    let str1 = return_string();
+    println!("{str1}");
+
+    let str2 = String::from("String for printing inside function");
+    print_str_by_referal(&str2);
+
+    let str3: String = String::from("String to modify and print");
+    append_no_modify_print(&str3);
+}
+
+fn return_string() -> String {
+    let z: String = String::from("... return string from funciton");
+    z
+}
+
+fn print_str_by_referal(str: &String) {
+    println!("{str}");
+}
+
+fn append_no_modify_print(str: &String) {
+    let mut temp_str: String = String::new();
+    temp_str.push_str(str);
+    temp_str.push_str("[THIS SECTION FROM FUNCTION]");
+    println!("{temp_str}");
 }
