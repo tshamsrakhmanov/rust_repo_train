c4a67b44a802622c00d422adcb876263bc625704
timur <ololokzn@gmail.com>
2025-07-31 13:09:16 +0000

    add user structure

--- struct_test/src/main.rs
+++ struct_test/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let mut new_user = User {
+        name: String::from("asdfasfd"),
+        surname: String::from("qwerqwer"),
+        active: true,
+        log_in_count: 5,
+    };
+
+    new_user.surname = String::from("qweqwer");
+
+    // println!("{new_user}");
+}
+
+struct User {
+    name: String,
+    log_in_count: i32,
+    surname: String,
+    active: bool,
 }
