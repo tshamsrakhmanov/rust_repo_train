678f3fe95fefe06094b560f0f5a6debeeddc8012
timur <ololokzn@gmail.com>
2025-08-01 11:45:54 +0000

    enum add some enums

--- enum_test/src/main.rs
+++ enum_test/src/main.rs
@@ -3,10 +3,29 @@ fn main() {
     let var2 = SomeEnum::Value2;
     println!("{var1:?}");
     println!("{var2:?}");
+    let var3 = MovementType::Running(14);
+    let var4 = MovementType::Trolling(12);
+    let var5 = MovementType::Walking(20);
+    println!("{var3:?}");
+    println!("{var4:?}");
+    println!("{var5:?}");
+    let address_v4 = IpAddress::V4(String::from("12.12.12.12"));
 }
 
 #[derive(Debug)]
 enum SomeEnum {
     Value1,
     Value2,
+}
+
+#[derive(Debug)]
+enum MovementType {
+    Walking(u8),
+    Trolling(u8),
+    Running(u8),
+}
+
+#[derive(Debug)]
+enum IpAddress {
+    V4(String),
 }
