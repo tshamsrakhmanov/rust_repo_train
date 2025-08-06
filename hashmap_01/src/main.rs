82ee749e6b754aecf5128ac2506813a0233f8e63
timur <ololokzn@gmail.com>
2025-08-06 15:28:53 +0000

    add check of hash map values by match and Occupied and Vacant

--- hashmap_01/src/main.rs
+++ hashmap_01/src/main.rs
@@ -12,4 +12,16 @@ fn main() {
         hashmap1.insert(temp, i);
     }
     println!("{hashmap1:?}");
+
+    // print by getting value using key getter
+    for (k, _v) in &hashmap1 {
+        let val = hashmap1[k];
+        println!("{val}");
+    }
+
+    // check, if a key is Occupied or Vacant and return back a value
+    match hashmap1.entry(String::from("key2")) {
+        std::collections::hash_map::Entry::Occupied(some_value) => println!("{some_value:?}"),
+        std::collections::hash_map::Entry::Vacant(some_value) => println!("{some_value:?}"),
+    }
 }
