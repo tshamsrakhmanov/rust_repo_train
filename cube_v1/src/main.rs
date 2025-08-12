11ea991add189074f2e46fa5536bcbbe7af15f86
timur <ololokzn@gmail.com>
2025-08-12 19:12:55 +0000

    add drawing simple points on screen according to main axises

--- cube_v1/src/main.rs
+++ cube_v1/src/main.rs
@@ -1,125 +1,93 @@
-use nalgebra::{self as na, Vector4};
-fn main() {
-    // declare values of up vector
-    // in fact, can be removed due to usage only in debug mode
-    let up_vector_x: f64 = 0.0;
-    let up_vector_y: f64 = 0.0;
-    let up_vector_z: f64 = -1.0;
+// use bresenham as br;
+use crossterm::{
+    QueueableCommand,
+    cursor::{Hide, MoveTo, Show},
+    event::{Event, KeyCode, poll, read},
+    execute,
+    style::{Color, Print, SetForegroundColor},
+    terminal::{
+        Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode,
+        enable_raw_mode, size,
+    },
+};
+use nalgebra::Vector4;
+use std::{
+    io::{self, Write},
+    os::unix::thread,
+    time::Duration,
+};
 
-    // setup of projection matrices
-    // step 1 - model matrix
-    // ... this model in identity matrix - so it's main diagonal is full of 1's
-    // ... this is the representation of modification for the point
-    // ... as long as we dont have any modifications - we keep it as identity matrix
-    let model = na::Matrix4::identity();
+mod screen_engine;
 
-    // step 2 - view matrix
-    // ... this matrix gives us representation of view point and how its allocated in space of
-    // global coordinates
-    let view = na::Matrix4::look_at_rh(
-        &na::Point3::new(5.0, 5.0, 5.0),
-        &na::Point3::origin(),
-        &na::Vector3::new(up_vector_x, up_vector_y, up_vector_z),
-    );
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // setup frame time
+    let fps: f32 = 1.0;
+    let frame_time = 1000.0 / fps;
 
-    // step 3 - projection matrix
-    // ... with this matrix we're making NDC area - so the projected image will be ready to apply
-    // to any given screen knowing  it's dimension parameters
-    // in this case i'll take 100 by 50 entries of terminal symbols
-    let screen_x_dimension: f64 = 100.0;
-    let screen_y_dimension: f64 = 50.0;
+    // setup screen size
+    let size = size()?;
+    let x_dim = size.0 as f64;
+    let y_dim = size.1 as f64;
 
-    // we need to setup boundaries as half of the screen, because we need it to face in the center
-    // of the view vector
-    let left_boundary = -1.0 * (&screen_x_dimension / 2.0);
-    let right_boundary = 1.0 * (&screen_x_dimension / 2.0);
-    let bottom_boundary = -1.0 * (&screen_y_dimension / 2.0);
-    let top_boundary = 1.0 * (&screen_y_dimension / 2.0);
+    // part of main loop where we calculate points on the screen
 
-    // calculation of the matrix itself
-    let projection = na::Matrix4::new_orthographic(
-        left_boundary,
-        right_boundary,
-        bottom_boundary,
-        top_boundary,
-        0.1,
-        100.0,
-    );
+    let mut points_cloud: Vec<(u32, u32)> = Vec::new();
 
-    // FINAL step - making MVP matrix - model X view X projection
-    let mvp = projection * view * model;
-
-    // by this point all neccessry matrix calcalutions are done
-    // now we can project points, vector - all we need - to the screen
-
-    // println!("MVP matrix: {}", mvp);
-
-    // make 4 points
     let point1 = Vector4::new(0.0, 0.0, 0.0, 1.0);
-    let point2 = Vector4::new(10.0, 0.0, 0.0, 1.0);
-    let point3 = Vector4::new(0.0, 10.0, 0.0, 1.0);
+    let point2 = Vector4::new(20.0, 0.0, 0.0, 1.0);
+    let point3 = Vector4::new(0.0, 20.0, 0.0, 1.0);
     let point4 = Vector4::new(0.0, 0.0, 10.0, 1.0);
+    let projection1 = screen_engine::calculate(x_dim, y_dim, point1);
+    let projection2 = screen_engine::calculate(x_dim, y_dim, point2);
+    let projection3 = screen_engine::calculate(x_dim, y_dim, point3);
+    let projection4 = screen_engine::calculate(x_dim, y_dim, point4);
 
-    // println!("desired point to draw:");
-    // println!("{point1}");
+    points_cloud.push(projection1);
+    points_cloud.push(projection2);
+    points_cloud.push(projection3);
+    points_cloud.push(projection4);
 
-    // make projection of the points
-    let point1_proj = mvp * point1;
-    let point2_proj = mvp * point2;
-    let point3_proj = mvp * point3;
-    let point4_proj = mvp * point4;
+    // prepare drawing engine
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, Hide)?;
 
-    // println!("result of drawing:");
-    // println!("{point1_proj}");
+    // main drawing loop
+    'main_loop: loop {
+        // Handle input
+        if poll(std::time::Duration::from_millis(frame_time as u64))? {
+            if let Event::Key(key_event) = read()? {
+                if key_event.code == KeyCode::Char('q') {
+                    break 'main_loop;
+                }
+            }
+        }
 
-    // remove W paramets - so make Vec3 from Vec4
-    let point1_ndc = point1_proj.xyz() / point1.w;
-    let point2_ndc = point2_proj.xyz() / point2.w;
-    let point3_ndc = point3_proj.xyz() / point3.w;
-    let point4_ndc = point4_proj.xyz() / point4.w;
+        // draw frame
+        draw_frame(&mut stdout, &points_cloud)?;
+    }
 
-    // println!("result of ndc:");
-    // println!("{point1_ndc}");
+    // clean up terminal
+    execute!(stdout, Show, LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+    Ok(())
+}
 
-    // calculation of RAW points parameters in coordinates of screen
-    // ATTENTION!
-    // it raw and based on up_vector parameters
-    // be ready to apply some changes if something is wrong with left-right-top-bottom on the
-    // screen
-    let x1_raw = (screen_x_dimension / 2.0) * (1.0 + point1_ndc.x);
-    let x2_raw = (screen_x_dimension / 2.0) * (1.0 + point2_ndc.x);
-    let x3_raw = (screen_x_dimension / 2.0) * (1.0 + point3_ndc.x);
-    let x4_raw = (screen_x_dimension / 2.0) * (1.0 + point4_ndc.x);
-    let y1_raw = (screen_y_dimension / 2.0) * (1.0 + point1_ndc.y);
-    let y2_raw = (screen_y_dimension / 2.0) * (1.0 + point2_ndc.y);
-    let y3_raw = (screen_y_dimension / 2.0) * (1.0 + point3_ndc.y);
-    let y4_raw = (screen_y_dimension / 2.0) * (1.0 + point4_ndc.y);
+fn draw_frame(
+    stdout: &mut io::Stdout,
+    points_cloud: &Vec<(u32, u32)>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    // Clear screen and draw new content
+    stdout.queue(Clear(ClearType::All))?;
 
-    // making horizontal flip
-    let x1 = screen_x_dimension - x1_raw;
-    let x2 = screen_x_dimension - x2_raw;
-    let x3 = screen_x_dimension - x3_raw;
-    let x4 = screen_x_dimension - x4_raw;
+    for point in points_cloud {
+        stdout.queue(MoveTo(point.0 as u16, point.1 as u16))?;
+        stdout.queue(SetForegroundColor(Color::White))?;
+        stdout.queue(Print("█"))?;
+    }
 
-    // this section is commented due to non-viable vertical flip
-    // let y1 = screen_y_dimension - y1_raw;
-    // let y2 = screen_y_dimension - y2_raw;
-    // let y3 = screen_y_dimension - y3_raw;
-    // let y4 = screen_y_dimension - y4_raw;
+    // Flush all commands at once
+    stdout.flush()?;
 
-    // this section commented because:
-    // it debug info about RAW points - before horizontal flip
-    // println!(
-    // "raw with up vector x:{} y:{},z:{}",
-    // &up_vector_x, &up_vector_y, &up_vector_z
-    // );
-    // println!("p1 x:{}, y:{} ", &x1_raw.round(), &y1_raw.round());
-    // println!("p2 x:{}, y:{} ", &x2_raw.round(), &y2_raw.round());
-    // println!("p3 x:{}, y:{} ", &x3_raw.round(), &y3_raw.round());
-    // println!("p4 x:{}, y:{} ", &x4_raw.round(), &y4_raw.round());
-    println!("ready to draw:");
-    println!("p1 x:{}, y:{} ", &x1.round(), &y1_raw.round());
-    println!("p2 x:{}, y:{} ", &x2.round(), &y2_raw.round());
-    println!("p3 x:{}, y:{} ", &x3.round(), &y3_raw.round());
-    println!("p4 x:{}, y:{} ", &x4.round(), &y4_raw.round());
+    Ok(())
 }
---
+++ cube_v1/src/screen_engine.rs
@@ -0,0 +1,37 @@
+use nalgebra::{self as na, Point2, Vector4};
+
+pub fn calculate(
+    screen_x_dimension: f64,
+    screen_y_dimension: f64,
+    point_input: Vector4<f64>,
+) -> (u32, u32) {
+    let model: na::Matrix4<f64> = na::Matrix4::identity();
+
+    let left_boundary = -1.0 * (&screen_x_dimension / 2.0);
+    let right_boundary = 1.0 * (&screen_x_dimension / 2.0);
+    let bottom_boundary = -1.0 * (&screen_y_dimension / 2.0);
+    let top_boundary = 1.0 * (&screen_y_dimension / 2.0);
+    let view = na::Matrix4::look_at_rh(
+        &na::Point3::new(5.0, 5.0, 5.0),
+        &na::Point3::origin(),
+        &na::Vector3::new(0.0, 0.0, -1.0),
+    );
+    let projection = na::Matrix4::new_orthographic(
+        left_boundary,
+        right_boundary,
+        bottom_boundary,
+        top_boundary,
+        0.1,
+        100.0,
+    );
+    let mvp = projection * view * model;
+
+    let point_proj = mvp * point_input;
+
+    let point_ndc = point_proj.xyz() / point_input.w;
+    let x1_raw = (screen_x_dimension / 2.0) * (1.0 + point_ndc.x);
+    let y1_raw = (screen_y_dimension / 2.0) * (1.0 + point_ndc.y);
+    let x1 = screen_x_dimension - x1_raw;
+
+    (x1 as u32, y1_raw as u32)
+}
