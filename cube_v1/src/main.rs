3605cb46db464afaec457720c3e9108f5c4791a5
timur <ololokzn@gmail.com>
2025-08-12 20:54:26 +0000

    examples of rotation - no success

--- cube_v1/src/main.rs
+++ cube_v1/src/main.rs
@@ -1,4 +1,5 @@
 // use bresenham as br;
+use core::f32;
 use crossterm::{
     QueueableCommand,
     cursor::{Hide, MoveTo, Show},
@@ -10,18 +11,19 @@ use crossterm::{
         enable_raw_mode, size,
     },
 };
-use nalgebra::Vector4;
+use nalgebra::{Point3, Rotation3, Vector3, Vector4};
 use std::{
+    f32,
     io::{self, Write},
-    os::unix::thread,
     time::Duration,
 };
 
 mod screen_engine;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    std::thread::sleep(Duration::from_millis(100));
     // setup frame time
-    let fps: f32 = 1.0;
+    let fps: f32 = 25.0;
     let frame_time = 1000.0 / fps;
 
     // setup screen size
@@ -31,26 +33,48 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // part of main loop where we calculate points on the screen
 
-    let mut points_cloud: Vec<(u32, u32)> = Vec::new();
-
-    let point1 = Vector4::new(0.0, 0.0, 0.0, 1.0);
-    let point2 = Vector4::new(20.0, 0.0, 0.0, 1.0);
-    let point3 = Vector4::new(0.0, 20.0, 0.0, 1.0);
-    let point4 = Vector4::new(0.0, 0.0, 10.0, 1.0);
-
-    let rotation_axis = Vector4::new(0.0, 1.0, 0.0, 0.0);
-
-    let projection1 = screen_engine::calculate(x_dim, y_dim, point1);
-    let projection2 = screen_engine::calculate(x_dim, y_dim, point2);
-    let projection3 = screen_engine::calculate(x_dim, y_dim, point3);
-    let projection4 = screen_engine::calculate(x_dim, y_dim, point4);
-
-    points_cloud.push(projection1);
-    points_cloud.push(projection2);
-    points_cloud.push(projection3);
-    points_cloud.push(projection4);
-
-    // prepare rotation axis - Y
+    // let mut points_cloud: Vec<(u32, u32)> = Vec::new();
+    //
+    // let edge: f64 = 10.0;
+    //
+    // let point1 = Vector4::new(edge, edge, edge, 1.0);
+    // let point2 = Vector4::new(-edge, -edge, edge, 1.0);
+    // let point3 = Vector4::new(-edge, edge, edge, 1.0);
+    // let point4 = Vector4::new(edge, -edge, edge, 1.0);
+    // let point5 = Vector4::new(edge, edge, -edge, 1.0);
+    // let point6 = Vector4::new(-edge, -edge, -edge, 1.0);
+    // let point7 = Vector4::new(-edge, edge, -edge, 1.0);
+    // let point8 = Vector4::new(edge, -edge, -edge, 1.0);
+    //
+    // // let rotation_axis = Vector4::new(0.0, 1.0, 0.0, 0.0);
+    // // let axis_angle = Vector3::z() * f32::consts::FRAC_PI_8;
+    // // let rot = Rotation3::new(axis_angle);
+    // // let rotated_point = rot * Point3::new(point5.x, point5.y, point5.z);
+    // // println!("{rotated_point}");
+    // // std::thread::sleep(Duration::from_millis(10000));
+    //
+    // let projection1 = screen_engine::calculate(x_dim, y_dim, point1);
+    // let projection2 = screen_engine::calculate(x_dim, y_dim, point2);
+    // let projection3 = screen_engine::calculate(x_dim, y_dim, point3);
+    // let projection4 = screen_engine::calculate(x_dim, y_dim, point4);
+    // let projection5 = screen_engine::calculate(x_dim, y_dim, point5);
+    // let projection6 = screen_engine::calculate(x_dim, y_dim, point6);
+    // let projection7 = screen_engine::calculate(x_dim, y_dim, point7);
+    // let projection8 = screen_engine::calculate(x_dim, y_dim, point8);
+    //
+    // points_cloud.push(projection1);
+    // points_cloud.push(projection2);
+    // points_cloud.push(projection3);
+    // points_cloud.push(projection4);
+    // points_cloud.push(projection5);
+    // points_cloud.push(projection6);
+    // points_cloud.push(projection7);
+    // points_cloud.push(projection8);
+    //
+    // prepare variable to store rotation factor
+    let mut factor: u8 = 0;
+    let axis_angle = Vector3::z() * f32::consts::FRAC_PI_8;
+    let rot = Rotation3::new(axis_angle);
 
     // prepare drawing engine
     enable_raw_mode()?;
@@ -67,6 +91,47 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 }
             }
         }
+
+        let mut points_cloud: Vec<(u32, u32)> = Vec::new();
+
+        let edge: f64 = 10.0;
+
+        let point1 = Vector4::new(edge, edge, edge, 1.0);
+        let point2 = Vector4::new(-edge, -edge, edge, 1.0);
+        let point3 = Vector4::new(-edge, edge, edge, 1.0);
+        let point4 = Vector4::new(edge, -edge, edge, 1.0);
+        let point5 = Vector4::new(edge, edge, -edge, 1.0);
+        let point6 = Vector4::new(-edge, -edge, -edge, 1.0);
+        let point7 = Vector4::new(-edge, edge, -edge, 1.0);
+        let point8 = Vector4::new(edge, -edge, -edge, 1.0);
+
+        // let rotation_axis = Vector4::new(0.0, 1.0, 0.0, 0.0);
+        // let axis_angle = Vector3::z() * f32::consts::FRAC_PI_8;
+        // let rot = Rotation3::new(axis_angle);
+        // let rotated_point = rot * Point3::new(point5.x, point5.y, point5.z);
+        // println!("{rotated_point}");
+        // std::thread::sleep(Duration::from_millis(10000));
+
+        let rot = Rotation3::from_axis_angle(&Vector3::z_axis(), f32::consts::FRAC_PI_6);
+        let rot_point = rot * point1;
+
+        let projection1 = screen_engine::calculate(x_dim, y_dim, point1);
+        let projection2 = screen_engine::calculate(x_dim, y_dim, point2);
+        let projection3 = screen_engine::calculate(x_dim, y_dim, point3);
+        let projection4 = screen_engine::calculate(x_dim, y_dim, point4);
+        let projection5 = screen_engine::calculate(x_dim, y_dim, point5);
+        let projection6 = screen_engine::calculate(x_dim, y_dim, point6);
+        let projection7 = screen_engine::calculate(x_dim, y_dim, point7);
+        let projection8 = screen_engine::calculate(x_dim, y_dim, point8);
+
+        points_cloud.push(projection1);
+        points_cloud.push(projection2);
+        points_cloud.push(projection3);
+        points_cloud.push(projection4);
+        points_cloud.push(projection5);
+        points_cloud.push(projection6);
+        points_cloud.push(projection7);
+        points_cloud.push(projection8);
 
         // draw frame
         draw_frame(&mut stdout, &points_cloud)?;
