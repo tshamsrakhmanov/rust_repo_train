ba69a9059b59a5861ca31ab16b13008358eef5fc
timur <ololokzn@gmail.com>
2025-08-19 21:53:46 +0000

    add visibility function of a triangle

--- geometry_test/src/geometry_engine.rs
+++ geometry_test/src/geometry_engine.rs
@@ -1,5 +1,6 @@
+use nalgebra::Vector3;
 use nalgebra::Vector4;
-use std::fmt;
+use std::fmt::{self, write};
 
 pub struct Triangle {
     point0: Vector4<f64>,
@@ -8,11 +9,11 @@ pub struct Triangle {
 }
 
 impl Triangle {
-    pub fn new(p1: Vector4<f64>, p2: Vector4<f64>, p3: Vector4<f64>) -> Triangle {
+    pub fn new(p0: Vector4<f64>, p1: Vector4<f64>, p2: Vector4<f64>) -> Triangle {
         let r = Triangle {
-            point0: p1,
-            point1: p2,
-            point2: p3,
+            point0: p0,
+            point1: p1,
+            point2: p2,
         };
 
         r
@@ -23,6 +24,20 @@ impl Triangle {
         r.push(self.point1);
         r.push(self.point2);
         r
+    }
+
+    pub fn is_visible(&self, pov: Vector4<f64>) -> bool {
+        let mut answer = false;
+        let v1 = Vector3::new(self.point0.x, self.point0.y, self.point0.z);
+        let v2 = Vector3::new(self.point1.x, self.point1.y, self.point1.z);
+        let v1_cross_v2 = v1.cross(&v2);
+        let v3 = Vector4::new(v1_cross_v2.x, v1_cross_v2.y, v1_cross_v2.z, 0.0);
+        let angle_rad = v3.angle(&pov);
+        let angle_deg = angle_rad.to_degrees();
+        if angle_deg < 90.0 {
+            answer = true;
+        }
+        answer
     }
 }
 
@@ -72,9 +87,9 @@ impl Pyramid {
     pub fn get_triangles(&self) -> Vec<Triangle> {
         let mut r = Vec::new();
         let t0: Triangle = Triangle::new(self.point0, self.point1, self.point2);
-        let t1: Triangle = Triangle::new(self.point0, self.point2, self.point3);
-        let t2: Triangle = Triangle::new(self.point1, self.point2, self.point3);
-        let t3: Triangle = Triangle::new(self.point0, self.point1, self.point3);
+        let t1: Triangle = Triangle::new(self.point2, self.point1, self.point3);
+        let t2: Triangle = Triangle::new(self.point0, self.point2, self.point3);
+        let t3: Triangle = Triangle::new(self.point1, self.point0, self.point3);
         r.push(t0);
         r.push(t1);
         r.push(t2);
