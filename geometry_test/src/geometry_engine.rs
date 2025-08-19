cf99504865f21ac92cf80d92cd0d4a78d9aecf2a
timur <ololokzn@gmail.com>
2025-08-19 10:45:35 +0000

    add usage of trait Display

--- geometry_test/src/geometry_engine.rs
+++ geometry_test/src/geometry_engine.rs
@@ -1,34 +1,26 @@
 use nalgebra::Vector4;
+use std::fmt;
 
-pub struct Pyramid {
+pub struct Triangle {
     point1: Vector4<f64>,
     point2: Vector4<f64>,
     point3: Vector4<f64>,
-    point4: Vector4<f64>,
 }
 
-impl Pyramid {
-    pub fn get_points(&self) -> Vec<Vector4<f64>> {
-        let mut r = Vec::new();
-        r.push(self.point1);
-        r.push(self.point2);
-        r.push(self.point3);
-        r.push(self.point4);
-        r
-    }
-    pub fn new(p1: Vector4<f64>, p2: Vector4<f64>, p3: Vector4<f64>, p4: Vector4<f64>) -> Pyramid {
-        let r = Pyramid {
+impl Triangle {
+    pub fn new(p1: Vector4<f64>, p2: Vector4<f64>, p3: Vector4<f64>) -> Triangle {
+        let r = Triangle {
             point1: p1,
             point2: p2,
             point3: p3,
-            point4: p4,
         };
+
         r
     }
-    pub fn rot_z(&mut self) {
-        self.point1.x = 1.0;
-        self.point2.x = 1.0;
-        self.point3.x = 1.0;
-        self.point4.x = 1.0;
+}
+
+impl fmt::Display for Triangle {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {}, {})", self.point1, self.point2, self.point3)
     }
 }
--- geometry_test/src/main.rs
+++ geometry_test/src/main.rs
@@ -1,22 +1,11 @@
 use nalgebra::Vector4;
 
-use crate::geometry_engine::Pyramid;
-
 mod geometry_engine;
 fn main() {
-    let p1 = Vector4::new(10.0, 10.0, 10.0, 1.0);
-    let p2 = Vector4::new(20.0, 20.0, 20.0, 1.0);
-    let p3 = Vector4::new(30.0, 30.0, 30.0, 1.0);
-    let p4 = Vector4::new(40.0, 40.0, 40.0, 1.0);
-    let mut p = geometry_engine::Pyramid::new(p1, p2, p3, p4);
-    let points = p.get_points();
-    for pos in points {
-        println!("{pos}");
-    }
-    p.rot_z();
-    let points = p.get_points();
-    for pos in points {
-        println!("{pos}");
-    }
-    p = Pyramid::new(p1, p2, p3, p4);
+    let p1 = Vector4::new(10.0, 0.0, 0.0, 1.0);
+    let p2 = Vector4::new(0.0, 10.0, 0.0, 1.0);
+    let p3 = Vector4::new(0.0, 0.0, 10.0, 1.0);
+
+    let tri = geometry_engine::Triangle::new(p1, p2, p3);
+    println!("{tri}");
 }
