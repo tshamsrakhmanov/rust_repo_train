b8bde2f9e48f2543372717393c11a54125e87d68
timur <ololokzn@gmail.com>
2025-08-23 08:57:28 +0000

    some sort of working algorythm of trianlge drawing with top-left rule

--- terminal_test/src/main.rs
+++ terminal_test/src/main.rs
@@ -29,8 +29,8 @@ fn main() -> io::Result<()> {
 
     let left = -dim_x / 2.0;
     let right = dim_x / 2.0;
-    let bottom = -dim_y / 1.4;
-    let top = dim_y / 1.4;
+    let bottom = -dim_y / 1.0;
+    let top = dim_y / 1.0;
     let znear = 0.0;
     let zfar = 1.0;
 
@@ -50,7 +50,7 @@ fn main() -> io::Result<()> {
     let pvm_matrix = projection_matrix * view_matrix * model1;
 
     // preparation of pyramid model
-    let edge = 14.0;
+    let edge = 24.0;
     let p0 = Vector4::new(0.0, edge, edge, 1.0);
     let p1 = Vector4::new(0.0, -edge, edge, 1.0);
     let p2 = Vector4::new(edge, 0.0, -edge, 1.0);
@@ -73,12 +73,12 @@ fn main() -> io::Result<()> {
     //  LOOP START
 
     // let rotation_vector = Vector4::new(0.2, 0.2, 0.2, 0.0);
-    let angle_deg = 8.55;
+    let angle_deg = 2.55;
     let mut screen_buffer: Vec<(u16, u16, u16)> = Vec::new();
     let pov_vec4 = Vector4::new(pov_x, pov_y, pov_z, 0.0);
 
     'main_loop: loop {
-        if poll(std::time::Duration::from_millis(40))? {
+        if poll(std::time::Duration::from_millis(5))? {
             if let Event::Key(key_event) = read()? {
                 if key_event.code == KeyCode::Char('q') {
                     break 'main_loop;
@@ -93,14 +93,16 @@ fn main() -> io::Result<()> {
             rand::random_range(0.0..1.0),
             0.0,
         );
-        // let rotation_vector = Vector4::new(0.0, 0.0, 1.0, 0.0);
+        // variable to store basic x-axis vector for rotation
+        // let rotation_vector1 = Vector4::new(0.1, 0.1, 0.1, 0.0);
+
         pyramid.rotate_by_vec4_mut(rotation_vector, angle_deg);
 
-        for trianle in pyramid.get_triangles() {
-            if trianle.is_visible(&pov_vec4) {
-                let p0_raw = trianle.point0;
-                let p1_raw = trianle.point1;
-                let p2_raw = trianle.point2;
+        for triangle in pyramid.get_triangles() {
+            if triangle.is_visible(&pov_vec4) {
+                let p0_raw = triangle.point0;
+                let p1_raw = triangle.point1;
+                let p2_raw = triangle.point2;
                 let prj_0 = projection(terminal_size.0, terminal_size.1, &pvm_matrix, &p0_raw);
                 let prj_1 = projection(terminal_size.0, terminal_size.1, &pvm_matrix, &p1_raw);
                 let prj_2 = projection(terminal_size.0, terminal_size.1, &pvm_matrix, &p2_raw);
@@ -109,48 +111,13 @@ fn main() -> io::Result<()> {
                     (prj_0.0 as i16, prj_0.1 as i16),
                     (prj_1.0 as i16, prj_1.1 as i16),
                     (prj_2.0 as i16, prj_2.1 as i16),
-                    trianle.color,
+                    triangle.color,
                 );
 
                 let points_from_triangle = tr1.get_drawing_points();
                 for p in points_from_triangle {
                     screen_buffer.push(p);
                 }
-
-                // let br0 = bresenham::Bresenham::new(
-                //     (prj_0.0 as isize, prj_0.1 as isize),
-                //     (prj_1.0 as isize, prj_1.1 as isize),
-                // );
-                // let br1 = bresenham::Bresenham::new(
-                //     (prj_1.0 as isize, prj_1.1 as isize),
-                //     (prj_2.0 as isize, prj_2.1 as isize),
-                // );
-                // let br2 = bresenham::Bresenham::new(
-                //     (prj_2.0 as isize, prj_2.1 as isize),
-                //     (prj_0.0 as isize, prj_0.1 as isize),
-                // );
-                //
-                // for pos in br0 {
-                //     let x = pos.0 as u16;
-                //     let y = pos.1 as u16;
-                //     screen_buffer.push((x, y));
-                // }
-                // for pos in br1 {
-                //     let x = pos.0 as u16;
-                //     let y = pos.1 as u16;
-                //     screen_buffer.push((x, y));
-                // }
-                // for pos in br2 {
-                //     let x = pos.0 as u16;
-                //     let y = pos.1 as u16;
-                //     screen_buffer.push((x, y));
-                // }
-                //
-                // for point in trianle.get_points() {
-                //     let prj = projection(terminal_size.0, terminal_size.1, &pvm_matrix, &point);
-                //
-                //     screen_buffer.push(prj);
-                // }
             }
         }
 
@@ -394,33 +361,48 @@ impl TriangleV2 {
         let max_x = cmp::max(cmp::max(self.point0.0, self.point1.0), self.point2.0);
         let min_y = cmp::min(cmp::min(self.point0.1, self.point1.1), self.point2.1);
         let max_y = cmp::max(cmp::max(self.point0.1, self.point1.1), self.point2.1);
-        let base_vector0 = Vector3::new(
+        let edge0_v3 = Vector3::new(
             (self.point1.0 - self.point0.0) as f64,
             (self.point1.1 - self.point0.1) as f64,
             0.0,
         );
-        let base_vector1 = Vector3::new(
+        let edge1_v3 = Vector3::new(
             (self.point2.0 - self.point1.0) as f64,
             (self.point2.1 - self.point1.1) as f64,
             0.0,
         );
-        let base_vector2 = Vector3::new(
+        let edge2_v3 = Vector3::new(
             (self.point0.0 - self.point2.0) as f64,
             (self.point0.1 - self.point2.1) as f64,
             0.0,
         );
+        let top_left_rule_parameter_0 = is_top_left(edge0_v3, edge1_v3);
+        let top_left_rule_parameter_1 = is_top_left(edge1_v3, edge2_v3);
+        let top_left_rule_parameter_2 = is_top_left(edge2_v3, edge0_v3);
+        let mut bias_0 = 0.0;
+        let mut bias_1 = 0.0;
+        let mut bias_2 = 0.0;
+        if top_left_rule_parameter_0 == false {
+            bias_0 = -1.0;
+        }
+        if top_left_rule_parameter_1 == false {
+            bias_1 = -1.0;
+        }
+        if top_left_rule_parameter_2 == false {
+            bias_2 = -1.0;
+        }
         for x in min_x..max_x + 1 {
             for y in min_y..max_y + 1 {
-                let temp_vector0 =
+                let p_coor_0 =
                     Vector3::new((x - self.point0.0) as f64, (y - self.point0.1) as f64, 0.0);
-                let temp_vector1 =
+                let p_coor_1 =
                     Vector3::new((x - self.point1.0) as f64, (y - self.point1.1) as f64, 0.0);
-                let temp_vector2 =
+                let p_coor_2 =
                     Vector3::new((x - self.point2.0) as f64, (y - self.point2.1) as f64, 0.0);
-                let w0 = temp_vector0.cross(&base_vector0).normalize();
-                let w1 = temp_vector1.cross(&base_vector1).normalize();
-                let w2 = temp_vector2.cross(&base_vector2).normalize();
-                if w2.z > 0.0 && w1.z > 0.0 && w0.z > 0.0 {
+                let w0_raw = p_coor_0.cross(&edge0_v3).normalize();
+                let w1_raw = p_coor_1.cross(&edge1_v3).normalize();
+                let w2_raw = p_coor_2.cross(&edge2_v3).normalize();
+                if w2_raw.z + bias_0 > 0.0 && w1_raw.z + bias_1 > 0.0 && w0_raw.z + bias_2 > 0.0 {
                     answer.push((x as u16, y as u16, self.color));
                 }
             }
@@ -428,4 +410,20 @@ impl TriangleV2 {
 
         answer
     }
+}
+fn is_top_left(vector0: Vector3<f64>, vector1: Vector3<f64>) -> bool {
+    let edge = Vector3::new(
+        (vector1.x - vector0.x) as f64,
+        (vector1.y - vector0.y) as f64,
+        0.0,
+    );
+    let mut a1 = true;
+    if edge.y == 0.0 && edge.x < 0.0 {
+        a1 = false;
+    }
+    let mut a2 = true;
+    if edge.y < 0.0 {
+        a2 = false;
+    }
+    a1 || a2
 }
