8249796590da96c109ec8df277d2a9d12f2b7fe6
timur <ololokzn@gmail.com>
2025-08-25 16:18:11 +0000

    triangle is working according to top left rule - but its garbage due to terminal drawing and will always be. to see the result of this algorythm - need to switch over to REAL graphics engine

--- terminal_test2/src/main.rs
+++ terminal_test2/src/main.rs
@@ -10,39 +10,45 @@ use crossterm::{
 use nalgebra::{self, Vector3};
 use std::cmp;
 use std::{
-    io::{Cursor, Write, stdout},
+    io::{Write, stdout},
     time::Duration,
 };
 
 fn main() -> std::io::Result<()> {
     let mut stdout = stdout();
 
-    let screen_size = terminal::size()?;
-
     let p0: Vector3<u16> = Vector3::new(10, 10, 0);
-    let p1: Vector3<u16> = Vector3::new(20, 10, 0);
-    let p2: Vector3<u16> = Vector3::new(10, 20, 0);
-    let p3: Vector3<u16> = Vector3::new(20, 20, 0);
+    let p1: Vector3<u16> = Vector3::new(10, 30, 0);
+    let p2: Vector3<u16> = Vector3::new(140, 10, 0);
+    let p3: Vector3<u16> = Vector3::new(140, 30, 0);
 
     let tr1 = TriangleV3::new(p0, p1, p2);
     let tr2 = TriangleV3::new(p1, p3, p2);
+
+    let mut screen_buffer: Vec<(u16, u16)> = Vec::new();
+
+    for pos in tr1.rasterize() {
+        screen_buffer.push(pos);
+    }
+
+    for pos in tr2.rasterize() {
+        screen_buffer.push(pos);
+    }
+
+    std::thread::sleep(Duration::from_millis(1000));
 
     execute!(stdout, cursor::Hide)?;
     execute!(stdout, EnterAlternateScreen)?;
     enable_raw_mode()?;
     'main_loop: loop {
-        for pos in tr1.rasterize() {
+        for pos in &screen_buffer {
             execute!(stdout, cursor::MoveTo(pos.0, pos.1))?;
             execute!(stdout, style::PrintStyledContent("O".yellow()))?;
         }
 
-        for pos in tr2.rasterize() {
-            execute!(stdout, cursor::MoveTo(pos.0, pos.1))?;
-            execute!(stdout, style::PrintStyledContent("O".blue()))?;
-        }
         stdout.flush()?;
 
-        if poll(Duration::from_millis(1000))? {
+        if poll(Duration::from_millis(10000))? {
             if let Event::Key(event) = read()? {
                 if event.code == KeyCode::Char('q') {
                     break 'main_loop;
@@ -66,6 +72,23 @@ impl TriangleV3 {
     fn rasterize(&self) -> Vec<(u16, u16)> {
         let mut answer: Vec<(u16, u16)> = Vec::new();
 
+        let self_p0_to_i16 = Vector3::new(
+            self.point0.x as i16,
+            self.point0.y as i16,
+            self.point0.z as i16,
+        );
+
+        let self_p1_to_i16 = Vector3::new(
+            self.point1.x as i16,
+            self.point1.y as i16,
+            self.point1.z as i16,
+        );
+
+        let self_p2_to_i16 = Vector3::new(
+            self.point2.x as i16,
+            self.point2.y as i16,
+            self.point2.z as i16,
+        );
         // make edge vectors
         let edge_0 = Vector3::new(
             self.point1.x as i16 - self.point0.x as i16,
@@ -94,28 +117,47 @@ impl TriangleV3 {
         // loop through region of intereset and check if points is compatible with conditions
         for x in min_x..max_x {
             for y in min_y..max_y {
-                // make local point vector according to eges
-                let edge_0_p = Vector3::new(
+                // make cross product of local point vector and edge vector
+
+                let v0 = Vector3::new(
                     x as i16 - self.point0.x as i16,
                     y as i16 - self.point0.y as i16,
                     0,
                 );
-                let edge_1_p = Vector3::new(
+
+                let v1 = Vector3::new(
                     x as i16 - self.point1.x as i16,
                     y as i16 - self.point1.y as i16,
                     0,
                 );
-                let edge_2_p = Vector3::new(
+
+                let v2 = Vector3::new(
                     x as i16 - self.point2.x as i16,
                     y as i16 - self.point2.y as i16,
                     0,
                 );
-                let edge_0_cross = edge_0.cross(&edge_0_p);
-                let edge_1_cross = edge_1.cross(&edge_1_p);
-                let edge_2_cross = edge_2.cross(&edge_2_p);
-                let w0 = edge_0_cross.z;
-                let w1 = edge_1_cross.z;
-                let w2 = edge_2_cross.z;
+
+                let edge_0_cross = v0.cross(&edge_0);
+                let edge_1_cross = v1.cross(&edge_1);
+                let edge_2_cross = v2.cross(&edge_2);
+                let tfl_param_0 = TriangleV3::tlf(&self_p0_to_i16, &self_p1_to_i16);
+                let tfl_param_1 = TriangleV3::tlf(&self_p2_to_i16, &self_p1_to_i16);
+                let tfl_param_2 = TriangleV3::tlf(&self_p0_to_i16, &self_p2_to_i16);
+                let mut w0 = edge_0_cross.z;
+                let mut w1 = edge_1_cross.z;
+                let mut w2 = edge_2_cross.z;
+                if !tfl_param_0 {
+                    w0 -= 1;
+                }
+
+                if !tfl_param_1 {
+                    w1 -= 1;
+                }
+
+                if !tfl_param_2 {
+                    w2 -= 1;
+                }
+                // making tlf variable and apply result to w-value
                 if w0 > 0 && w1 > 0 && w2 > 0 {
                     answer.push((x as u16, y as u16));
                 }
@@ -131,5 +173,18 @@ impl TriangleV3 {
             point2: point2,
         };
         a
+    }
+    fn tlf(start: &Vector3<i16>, end: &Vector3<i16>) -> bool {
+        let temp_vec = Vector3::new(end.x - start.x, end.y - start.y, 0);
+        let mut a1 = false;
+        if temp_vec.y == 0 && temp_vec.x > 0 {
+            a1 = true;
+        }
+        let mut a2 = false;
+        if temp_vec.y < 0 {
+            a2 = true;
+        }
+
+        a1 || a2
     }
 }
