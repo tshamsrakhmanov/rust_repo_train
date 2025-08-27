6a36b0bd7a506b3095a3508257963501ca567ac2
timur <ololokzn@gmail.com>
2025-08-27 08:57:45 +0000

    working algorythm with screen buffers - only need to change the way a color applied

--- flicker_free/src/main.rs
+++ flicker_free/src/main.rs
@@ -9,11 +9,11 @@ use crossterm::{
         enable_raw_mode, size,
     },
 };
-use std::time::Duration;
 use std::{
     collections::HashMap,
     io::{self, Write},
 };
+use std::{collections::HashSet, time::Duration};
 fn main() -> io::Result<()> {
     let mut stdout = io::stdout();
 
@@ -21,10 +21,10 @@ fn main() -> io::Result<()> {
     let dim_x = screen_size.0;
     let dim_y = screen_size.1;
 
-    let mut prev_screen_buffer: HashMap<(u16, u16), Color> = HashMap::new();
-    prev_screen_buffer = scr_0(dim_x, dim_y);
+    let mut prev_screen_buffer: HashMap<(u16, u16), Color> = gen_section(dim_x, dim_y);
+    let mut next_screen_buffer: HashMap<(u16, u16), Color> = HashMap::new();
 
-    let mut flag = false;
+    let mut flag = true;
 
     enable_raw_mode()?;
     execute!(io::stdout(), EnterAlternateScreen)?;
@@ -40,54 +40,53 @@ fn main() -> io::Result<()> {
             }
         }
 
-        // SECTION 1 - UPDATE GLOBAL BUFFER
-        let mut new_screen_buffer: HashMap<(u16, u16), Color> = HashMap::new();
+        // SECTION 1 - UPDATE NEXT BUFFER
         flag = !flag;
+
         if flag {
-            new_screen_buffer = scr_0(dim_x, dim_y);
+            next_screen_buffer = gen_full(dim_x, dim_y);
         } else {
-            new_screen_buffer = scr_1(dim_x, dim_y);
+            next_screen_buffer = gen_section(dim_x, dim_y);
         }
 
         // SECTION 2 - UPDATE DRAWING BUFFER, CONVERT TO STRING AND DRAW
-        let mut screen_buffer_text = String::new();
 
-        for y in 0..dim_y {
-            for x in 0..dim_x {
-                let temp_pos = (x, y);
-                let old_unwrap = prev_screen_buffer.get_key_value(&temp_pos);
-                let new_unwrap = new_screen_buffer.get_key_value(&temp_pos);
-                let mut is_in_old = false;
-                let mut is_in_new = false;
-                match old_unwrap {
-                    None => {}
-                    Some(_) => {
-                        is_in_old = true;
-                    }
-                }
-                match new_unwrap {
-                    None => {}
-                    Some(_) => {
-                        is_in_new = true;
-                    }
-                }
+        let keys_prev_buffer: HashSet<&(u16, u16)> = prev_screen_buffer.keys().collect();
+        let keys_next_buffer: HashSet<&(u16, u16)> = next_screen_buffer.keys().collect();
 
-                if is_in_old && is_in_new {
-                    //
-                } else if is_in_old && !is_in_new {
-                    execute!(stdout, cursor::MoveTo(x, y))?;
-                    execute!(stdout, style::PrintStyledContent(" ".magenta()))?;
-                    //
-                } else if !is_in_old && is_in_new {
-                    //
-                } else if !is_in_old && !is_in_new {
-                    //
-                }
-            }
+        let to_delete = keys_prev_buffer.difference(&keys_next_buffer);
+        let to_redraw = keys_prev_buffer.intersection(&keys_next_buffer);
+        let to_draw_new = keys_next_buffer.difference(&keys_prev_buffer);
+
+        for pos in to_delete {
+            execute!(stdout, cursor::MoveTo(pos.0, pos.1))?;
+            execute!(stdout, Print(" "))?;
+        }
+        for pos in to_redraw {
+            execute!(stdout, cursor::MoveTo(pos.0, pos.1))?;
+            execute!(stdout, Print("O"))?;
+        }
+        for pos in to_draw_new {
+            execute!(stdout, cursor::MoveTo(pos.0, pos.1))?;
+            execute!(stdout, Print("O"))?;
         }
 
-        execute!(stdout, cursor::MoveTo(0, 0))?;
-        execute!(stdout, Print(screen_buffer_text))?;
+        prev_screen_buffer = next_screen_buffer;
+
+        // execute!(stdout, Clear(ClearType::All))?;
+        //
+        // for y in 0..dim_y {
+        //     for x in 0..dim_x {
+        //         let c = next_screen_buffer.get_key_value(&(x, y));
+        //         match c {
+        //             None => {}
+        //             Some(_) => {
+        //                 execute!(stdout, cursor::MoveTo(x, y))?;
+        //                 execute!(stdout, Print("O"))?;
+        //             }
+        //         }
+        //     }
+        // }
 
         stdout.flush()?;
     }
@@ -104,30 +103,25 @@ enum Color {
     RED,
 }
 
-fn scr_0(dim_x: u16, dim_y: u16) -> HashMap<(u16, u16), Color> {
+fn gen_section(dim_x: u16, dim_y: u16) -> HashMap<(u16, u16), Color> {
     let mut screen_buffer: HashMap<(u16, u16), Color> = HashMap::new();
-
     for y in 0..dim_y {
         for x in 0..dim_x {
-            if x > 10 && x < dim_x - 10 {
-                screen_buffer.insert((x, y), Color::RED);
+            if x > 10 && x < 100 && y > 5 && y < 20 {
+                screen_buffer.insert((x, y), Color::WHITE);
             }
         }
     }
-
     screen_buffer
 }
-
-fn scr_1(dim_x: u16, dim_y: u16) -> HashMap<(u16, u16), Color> {
+fn gen_full(dim_x: u16, dim_y: u16) -> HashMap<(u16, u16), Color> {
     let mut screen_buffer: HashMap<(u16, u16), Color> = HashMap::new();
-
     for y in 0..dim_y {
         for x in 0..dim_x {
-            if x > 15 && x < dim_x - 15 {
-                screen_buffer.insert((x, y), Color::BLUE);
+            if x > 11 && x < 101 && y > 5 && y < 20 {
+                screen_buffer.insert((x, y), Color::WHITE);
             }
         }
     }
-
     screen_buffer
 }
