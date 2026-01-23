84b685c1c7d920e63e3ebef5196ae84a3ff63e3b
Tim <ololokzn@example.com>
2026-01-23 12:34:06 +0000

    add simple picture drawing - ppm. with counter

--- ray_tracer/src/main.rs
+++ ray_tracer/src/main.rs
@@ -1,22 +1,21 @@
 use std::fs::File;
-use std::io::{Bytes, prelude::*};
+use std::io::prelude::*;
 
 fn main() -> std::io::Result<()> {
     let mut file = File::create("pic.ppm")?;
     file.write_all(b"P3\n")?;
-    file.write_all(b"3 2\n")?;
+    file.write_all(b"255 255\n")?;
     file.write_all(b"255\n")?;
-    // file.write_all(b"255 0 0 0 255 0 0 0 255\n")?;
-    // file.write_all(b"255 255 0 255 255 255 0 0 0\n")?;
-    // Ok(())
-    //
-    let width = 255;
-    let height = 255;
 
-    for w_pos in 0..height {
-        for h_pos in 0..width {
-            let r_float: f32 = (w_pos as f32) / ((width as f32) - 1.0);
-            let g_float: f32 = (h_pos as f32) / ((height as f32) - 1.0);
+    let x_pos_limit = 255;
+    let y_pos_limit = 255;
+
+    for x_pos in 0..x_pos_limit {
+        let a = x_pos_limit - x_pos;
+        println!("Scan lines remaining: {}", a);
+        for y_pos in 0..y_pos_limit {
+            let r_float: f32 = (y_pos as f32) / ((x_pos_limit as f32) - 1.0);
+            let g_float: f32 = (x_pos as f32) / ((y_pos_limit as f32) - 1.0);
             let b_float: f32 = 0.0;
 
             let ir = (255.999 * r_float) as u8;
