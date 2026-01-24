389e30f3e88da3477d5916720552d53c5442902a
Tim <ololokzn@example.com>
2026-01-24 14:18:12 +0000

    rework of writer - again. now fast and convinient.

--- ray_tracer/src/main.rs
+++ ray_tracer/src/main.rs
@@ -2,10 +2,10 @@ use std::fs::File;
 use std::io::prelude::*;
 
 fn main() -> std::io::Result<()> {
-    // VAR - raw declaration
+    // var declaration
     let color_type = String::from("P3");
-    let x_dim = 500;
-    let y_dim = 255;
+    let x_dim = 400;
+    let y_dim = 200;
     let coloring_dim = 255;
 
     // file connect
@@ -16,6 +16,7 @@ fn main() -> std::io::Result<()> {
     write!(file, "{} {}\n", x_dim, y_dim)?;
     write!(file, "{}\n", coloring_dim)?;
 
+    // fill the space
     for y_pos in 0..y_dim {
         let a = y_dim - y_pos;
         println!("Scan lines remaining: {}", a);
@@ -24,18 +25,20 @@ fn main() -> std::io::Result<()> {
             let g_float: f32 = (y_pos as f32) / ((y_dim as f32) - 1.0);
             let b_float: f32 = 0.0;
 
-            let ir = (255.999 * r_float) as u8;
-            let ig = (255.999 * g_float) as u8;
-            let ib = (255.999 * b_float) as u8;
-            let str_to_print = format!("{} {} {}\n", ir, ig, ib);
-            let bytes: &[u8] = str_to_print.as_bytes();
-            file.write_all(bytes)?;
+            write_pixetl_to_file_clean(&mut file, r_float, g_float, b_float);
         }
     }
+    println!("Done!");
 
     Ok(())
 }
 
-fn write_pixel_to_file(file: &mut File, r: f32, g: f32, b: f32) {
-    let _ = write!(file, "{} {} {}\n", r, g, b);
+fn write_pixetl_to_file_clean(file: &mut File, r: f32, g: f32, b: f32) {
+    let ir = (255.999 * r) as u8;
+    let ig = (255.999 * g) as u8;
+    let ib = (255.999 * b) as u8;
+
+    let str1 = format!("{} {} {}\n", ir, ig, ib);
+    let byt1 = str1.as_bytes();
+    let _ = file.write(byt1);
 }
