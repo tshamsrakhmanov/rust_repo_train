41df2265406c1c1332dc17408152070060bb3752
Tim <ololokzn@example.com>
2026-01-24 13:43:07 +0000

    redo mistakes of scan lines

--- ray_tracer/src/main.rs
+++ ray_tracer/src/main.rs
@@ -11,12 +11,12 @@ fn main() -> std::io::Result<()> {
     let x_pos_limit = 255;
     let y_pos_limit = 255;
 
-    for x_pos in 0..x_pos_limit {
-        let a = x_pos_limit - x_pos;
+    for y_pos in 0..y_pos_limit {
+        let a = y_pos_limit - y_pos;
         println!("Scan lines remaining: {}", a);
-        for y_pos in 0..y_pos_limit {
-            let r_float: f32 = (y_pos as f32) / ((x_pos_limit as f32) - 1.0);
-            let g_float: f32 = (x_pos as f32) / ((y_pos_limit as f32) - 1.0);
+        for x_pos in 0..x_pos_limit {
+            let r_float: f32 = (x_pos as f32) / ((x_pos_limit as f32) - 1.0);
+            let g_float: f32 = (y_pos as f32) / ((y_pos_limit as f32) - 1.0);
             let b_float: f32 = 0.0;
 
             let ir = (255.999 * r_float) as u8;
