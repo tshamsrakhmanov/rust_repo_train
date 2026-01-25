c714daab7f6e6e1070ea8b507b310dfe732bbba7
Timur <ololokzn@gmail.com>
2026-01-25 19:57:40 +0000

    remained unsolved issue with hittable trait. TODO

--- ray_tracer/src/hittable.rs
+++ ray_tracer/src/hittable.rs
@@ -3,6 +3,33 @@ pub mod hittable {
 
     use crate::ray::ray_module::Ray;
 
+    pub struct Hittable_list {
+        objects: Vec<Hittable>,
+    }
+
+    impl Hittable_list {
+        pub fn add_object(&mut self, object: Hittable) {
+            self.objects.push(object);
+        }
+        pub fn clear(&mut self) {
+            self.objects.clear();
+        }
+    }
+
+    impl TraitHittable for Hittable_list {
+        fn hit(&self, ray: &Ray, ray_tmin: f32, ray_tmax: f32, rec: &mut Hittable) -> bool {
+            // true
+            let temp_rec = rec;
+            let mut hit_anything = false;
+            let closest_so_far = ray_tmax;
+
+            for obj in &self.objects {
+                let hitted = obj.
+            }
+
+            true
+        }
+    }
     pub struct Hittable {
         p: Vector3<f32>,
         normal: Vector3<f32>,
@@ -41,7 +68,10 @@ pub mod hittable {
         }
     }
 
+    impl TraitHittable for Hittable {}
+
     pub trait TraitHittable {
+        // fn hit() {};
         fn hit(&self, ray: &Ray, ray_tmin: f32, ray_tmax: f32, rec: &mut Hittable) -> bool;
     }
 }
