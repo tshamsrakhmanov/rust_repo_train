3990c7e73e88a76a5d76a29705d901fec01a99f4
Tim <ololokzn@example.com>
2026-01-26 17:13:48 +0000

    add function of distance of hit

--- trait_test/src/main.rs
+++ trait_test/src/main.rs
@@ -1,8 +1,9 @@
 use nalgebra::Vector3;
+use std::f32::INFINITY;
 
 fn main() {
-    let ray_origin = Vector3::new(-10.0, 0.0, 0.0);
-    let ray_direction = Vector3::new(0.0, 0.0, 0.0);
+    let ray_origin = Vector3::new(15.0, 0.0, 0.0);
+    let ray_direction = Vector3::new(1000.0, 0.0, 0.0);
     let r1 = Ray::new(ray_origin, ray_direction);
 
     let pos1 = Vector3::new(0.0, 0.0, 0.0);
@@ -20,10 +21,11 @@ fn main() {
     println!("{:?}", r1);
 
     for obj in &wrld1.list_of_objects {
-        // println!("{:?}", obj);
-        let a = obj.is_hit(&r1);
+        println!("------------");
+        let a = obj.is_hit(&r1, 0.0, INFINITY);
         if a.is_hit {
             println!("{:?} is hit!", obj);
+            println!("{:?} record is:", a.hit_record);
         } else {
             println!("{:?} NO hit", obj);
         }
@@ -90,11 +92,13 @@ impl Object {
 }
 
 impl Hittable for Object {
-    fn is_hit(&self, ray: &Ray) -> HitResultTuple {
-        let temp_hitrecord = HitRecord::new_default();
-        // let mut temp_hit_result_tuple = HitResultTuple::new(false, temp_hitrecord);
+    fn is_hit(&self, ray: &Ray, start: f32, finish: f32) -> HitResultTuple {
+        let mut temp_hitrecord = HitRecord::new_default();
 
-        if is_point_on_line(self.position, ray.origin, ray.direction, 0.001) {
+        let result_of_check = is_point_on_line(self.position, ray.origin, ray.direction, 0.001);
+
+        if result_of_check.0 {
+            temp_hitrecord.distance = result_of_check.1;
             let a = HitResultTuple::new(true, temp_hitrecord);
             return a;
         }
@@ -105,25 +109,25 @@ impl Hittable for Object {
 }
 
 fn is_point_on_line(
-    point: Vector3<f32>,
-    line_ori: Vector3<f32>,
-    line_dir: Vector3<f32>,
+    vec_c: Vector3<f32>,
+    vec_a: Vector3<f32>,
+    vec_b: Vector3<f32>,
     tolerance: f32,
-) -> bool {
-    // let d = line_dir.normalize();
-    // let v = point - line_ori;
-    // let distance = v.cross(&d).norm();
-    // distance < tolerance
-    let direction_vector = line_dir - line_ori;
-    // println!("{}", direction_vector);
-    let dir_of_point = point - line_ori;
-    // println!("{}", dir_of_point);
-    let crs_prd = dir_of_point.cross(&direction_vector).norm();
-    // println!("{} {} ", crs_prd, tolerance);
-    if crs_prd < tolerance {
-        return true;
+) -> (bool, f32) {
+    let vec_d = vec_b - vec_a;
+    // println!("direction line {:?}", vec_d);
+    let vec_e = vec_c - vec_a;
+    // println!("direction test point {:?}", vec_e);
+    let cross_product_length = vec_e.cross(&vec_d).norm();
+    let dot_product = vec_d.dot(&vec_e);
+    // println!("cross product {:?}", cross_product_length);
+    // println!("dot   product {:?}", dot_product);
+
+    if cross_product_length < tolerance && dot_product > 0.0 {
+        let distance_of_hit = vec_e.norm();
+        return (true, distance_of_hit);
     } else {
-        return false;
+        return (false, 0.0);
     }
 }
 
@@ -160,5 +164,5 @@ impl World {
 }
 
 trait Hittable {
-    fn is_hit(&self, ray: &Ray) -> HitResultTuple;
+    fn is_hit(&self, ray: &Ray, start: f32, finish: f32) -> HitResultTuple;
 }
